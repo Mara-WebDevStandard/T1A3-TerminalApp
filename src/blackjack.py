e7a8f7f71486af1913a245185cc6091ec74f3065
Marianna <marianna.ilisevic@gmail.com>
2023-07-16 02:01:48 +1000

    MVP - single player blackjack with human player.

--- src/blackjack.py
+++ src/blackjack.py
@@ -2,36 +2,35 @@
 
 from enum import Enum
 from random import shuffle
+from utilities import *
 
 
 class Suits(Enum):
-    Clubs = 0
-    Diamonds = 1
-    Hearts = 2
-    Spades = 3
+    clubs = 0
+    diamonds = 1
+    hearts = 2
+    spades = 3
 
 
 class Faces(Enum):
-    One = 1
-    Two = 2
-    Three = 3
-    Four = 4
-    Five = 5
-    Six = 6
-    Seven = 7
-    Eight = 8
-    Nine = 9
-    Ten = 10
-    Jack = 11
-    Queen = 12
-    Ace = 13
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    jack = 11
+    queen = 12
+    ace = 13
 
 
 class Action(Enum):
-    Hit = 1
-    Stand = 2
-    DoubleDown = 3
-    Split = 4
+    hit = 1
+    stand = 2
 
 
 class Card:
@@ -40,23 +39,25 @@ class Card:
         self.face = face
 
     def hard_value(self):
-        return 11 if self.face == Faces.Ace else min(self.face.value, 10)
+        return 11 if self.face == Faces.ace else min(self.face.value, 10)
 
     def long_name(self):
-        return f'{self.face.name} of {self.suit.name}'
+        return f'{self.face.name.capitalize()} of {self.suit.name.capitalize()}'
 
     def short_name(self):
-        suit = self.suit.name[0]
-        val = self.face.value if self.face.value <= 10 else self.face.name[0]
+        suit = self.suit.name[0].upper()
+        val = self.face.value if self.face.value <= 10 else self.face.name[0].upper()
         return f'{val}{suit}'
-
+    
+def hand_to_str(cards):
+    return "".join([f'[{c.short_name()}]' for c in cards])
 
 def hand_value(cards):
     value = 0
     n_aces = 0
 
     for c in cards:
-        if c.face == Faces.Ace:
+        if c.face == Faces.ace:
             n_aces += 1
         value += c.hard_value()
 
@@ -65,7 +66,6 @@ def hand_value(cards):
         n_aces -= 1
 
     return value
-
 
 def make_decks(count):
     cards = []
@@ -76,3 +76,157 @@ def make_decks(count):
                 cards.append(Card(s, f))
 
     return cards
+
+class Player:
+    def __init__(self, funds):
+        self.funds = funds
+        self.stood = False
+
+    def bet(self):
+        while True:
+            bet = read_num("Please enter a wager")
+
+            if bet < 0:
+                print("Unable to place a negative bet!")
+            elif bet > self.funds:
+                print("Not enough funds!")
+            else:
+                return bet  
+
+    def choice(self, pcards, dcards):
+        while True:
+            act = read_enum("Enter a choice", Action)
+            if act == Action.stand:
+                self.stood = True
+            return act
+        
+class Dealer:
+    def __init__(self, funds):
+        self.funds = funds
+        self.stood = False
+
+    def choice(self, pcards, dcards):
+        if hand_value(dcards) >= 17:
+            self.stood = True
+            return Action.stand
+        else:
+            return Action.hit
+
+# returns winnings amount to player (negative for a loss)
+def blackjack_round(deck, dealer, player, bet):
+
+    d_cards = [deck.pop(), deck.pop()]
+    p_cards = [deck.pop(), deck.pop()]
+
+    d_bjack = hand_value(d_cards) == 21
+    p_bjack = hand_value(p_cards) == 21
+    push = d_bjack and p_bjack
+
+    if d_bjack or p_bjack:
+        print(f'Dealer cards: {hand_to_str(d_cards)}')
+        print(f'Player cards: {hand_to_str(p_cards)}')  
+
+    if push: # break even
+       print('Player and dealer got blackjack')
+       return 0
+    elif p_bjack: # 3:2 payout for blackjack
+        print('Player got blackjack!')
+        return bet * 1.5
+    elif d_bjack:
+        print('Dealer got blackjack')
+        return -bet # lose bet
+        
+    #special case for hidden card on first round
+    print(f'Dealer cards: [{d_cards[0].short_name()}][**]')
+    print(f'Player cards: {hand_to_str(p_cards)}')
+
+    #give player option to double down
+    if (read_yn("Double down")):
+        bet *= 2
+        p_cards.append(deck.pop())
+
+        while not dealer.stood:
+            if dealer.choice(p_cards, d_cards) == Action.hit:
+                d_cards.append(deck.pop())
+        
+        print(f'Dealer cards: {hand_to_str(d_cards)}')
+        print(f'Player cards: {hand_to_str(p_cards)}')
+
+        p_val = hand_value(p_cards)
+        d_val = hand_value(d_cards)
+
+        if p_val > 21: # player always loses if bust - even if dealer busts too
+            print('Player went bust!')
+            return -bet
+        elif d_val > 21:
+            print('Dealer went bust!')
+            return bet
+        elif p_val > d_val:
+            return bet
+        elif p_val < d_val:
+            return -bet
+        else: # p_val == d_val
+            return 0
+
+    while True:
+
+        if not player.stood:
+            if player.choice(p_cards, d_cards) == Action.hit:
+                p_cards.append(deck.pop())
+                if hand_value(p_cards) > 21:
+                    print(f'Player cards: {hand_to_str(p_cards)}')
+                    print('Player went bust!')
+                    return -bet
+        
+
+        if not dealer.stood:
+            if dealer.choice(p_cards, d_cards) == Action.hit:
+                d_cards.append(deck.pop())
+                if hand_value(d_cards) > 21:
+                    print(f'Dealer cards: {hand_to_str(d_cards)}')
+                    print('Dealer went bust!')
+                    return bet
+
+        print(f'Dealer cards: {hand_to_str(d_cards)}')
+        print(f'Player cards: {hand_to_str(p_cards)}')
+
+        if dealer.stood and player.stood:
+            p_val = hand_value(p_cards)
+            d_val = hand_value(d_cards)
+
+            print(f'Player score: {p_val} | Dealer score: {d_val}')
+
+            if p_val > d_val:
+                return bet
+            elif p_val < d_val:
+                return -bet
+            else: # p_val == d_val
+                return 0
+                
+
+
+def blackjack_main():
+
+    deck = make_decks(5)
+    shuffle(deck)
+
+    dealer = Dealer(10000)
+    player = Player(500)
+
+    while True:
+        bet = player.bet()
+        winnings = blackjack_round(deck, dealer, player, bet)
+        player.funds += winnings
+        dealer.funds -= winnings
+
+        if (winnings > 0):
+            print(f'Player won! Winnings = ${winnings:.2f}')
+        elif winnings == 0:
+            print('Player tied! Winnings = $0.00')
+        else:
+            print(f'Player lost! Losses = ${-winnings:.2f}')
+        print(f'Player funds = ${player.funds} | Dealer funds = ${dealer.funds}')
+            
+
+if __name__ == "__main__":
+    blackjack_main()
--- src/test_blackjack.py
+++ src/test_blackjack.py
@@ -7,16 +7,14 @@ def test_hand_value():
     # autopep8: off
 
     # No aces
-    assert hand_value([Card(Suits.Clubs, Faces.Three),Card(Suits.Clubs, Faces.Jack)]) == 13
+    assert hand_value([Card(Suits.clubs, Faces.three),Card(Suits.clubs, Faces.jack)]) == 13
     # One hard ace
-    assert hand_value([Card(Suits.Clubs, Faces.Ace),Card(Suits.Clubs, Faces.Jack)]) == 21
+    assert hand_value([Card(Suits.clubs, Faces.ace),Card(Suits.clubs, Faces.jack)]) == 21
     # One soft ace
-    assert hand_value([Card(Suits.Clubs, Faces.Ace),Card(Suits.Clubs, Faces.Jack), Card(Suits.Clubs, Faces.Two)]) == 13
-
+    assert hand_value([Card(Suits.clubs, Faces.ace),Card(Suits.clubs, Faces.jack), Card(Suits.clubs, Faces.two)]) == 13
     # One soft and one hard ace
-    assert hand_value([Card(Suits.Clubs, Faces.Ace),Card(Suits.Clubs, Faces.Ace), Card(Suits.Clubs, Faces.Four)]) == 16
-
+    assert hand_value([Card(Suits.clubs, Faces.ace),Card(Suits.clubs, Faces.ace), Card(Suits.clubs, Faces.four)]) == 16
     # Two soft aces
-    assert hand_value([Card(Suits.Clubs, Faces.Ace), Card(Suits.Clubs, Faces.Ace), Card(Suits.Clubs, Faces.Ten)]) == 12
+    assert hand_value([Card(Suits.clubs, Faces.ace), Card(Suits.clubs, Faces.ace), Card(Suits.clubs, Faces.ten)]) == 12
 
     # autopep8: off
